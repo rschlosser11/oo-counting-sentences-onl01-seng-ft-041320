0d0b05a2261c5c0199b5b7a6dd9b977e6db97efd
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-20 15:58:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentence_arr = self.split(/[.?!]/)
+    i = 0
+    while i < sentence_arr.length
+      sentence_arr.delete_at(i) if sentence_arr[i] == '';
+      i += 1
+    end
+    sentence_arr.length
   end
 end
